9a9f0b58bb57b05b3f6a04e1fef11e9ee30bc1c4
Jack Goodman <jacknathgoodman@gmail.com>
2023-10-16 18:44:11 +0100

    created top-level displaydata class

--- src/main.cs
+++ src/main.cs
@@ -3,19 +3,13 @@ using System.Data;
 namespace DmgScy;
 
 public static class Run{
+    
     public static void Main(string[] args){
-        BandService bandService = new BandService();
-        bandService.DatabaseCreate();
-        bandService.DatabaseInsert();
-        Dictionary<string, DataTable?> dataTableWithHeader = bandService.DatabaseSelectWithHeader();
-        string header = dataTableWithHeader.First().Key;
-        DataTable? dataTable = dataTableWithHeader.First().Value;
-        var baseData = new PageData(Constants.Html.indexBase);
-        if(dataTable!= null){
-            baseData.WriteNewHTML(outfileLoc: Constants.Html.index, dataTable: dataTable, tableHeader: header);
-        }
-        var pageData = new PageData(Constants.Html.index);
-        var server = new Server(Constants.localhost, pageData.html);
+        PassDisplayData displayData = new PassDisplayData();
+        displayData.InitiateDatabase();
+        displayData.WriteBandsToIndexHtml();
+        PageData indexData = new PageData(Constants.Html.index);
+        Server server = new Server(Constants.localhost, indexData.html);
         server.listener.Start();
         Console.WriteLine("Listening for connections on {0}", Constants.localhost);
         server.HandleIncomingConnections().GetAwaiter().GetResult();
